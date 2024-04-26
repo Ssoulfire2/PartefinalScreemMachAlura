875198ef0e4de9ca847832d38bcac645833ef7af
Ssoulfire2 <alexcabral546@gmail.com>
2024-04-26 12:45:38 -0400

    Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,42 @@
+import com.aluracursos.screematch.calculos.CalculadoraDeTiempo;
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del dragon");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEspisodiosPorTemporradas(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getMinutosPorEpisodio());
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.setNombre("Matrix");
+        otraPelicula.setFechaDeLanzamiento(1998);
+        otraPelicula.setDuracionEnMinutos(180);
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        calculadora.incluye(otraPelicula);
+        System.out.println(calculadora.getTiempoTotal());
+
+
+    }
+}
---
+++ src/com/aluracursos/screematch/calculos/CalculadoraDeTiempo.java
@@ -0,0 +1,2 @@
+package com.aluracursos.screematch.calculos;public class calculadoraDeTiempo {
+}
